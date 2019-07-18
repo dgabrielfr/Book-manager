c9feb7c2d0470c516ba48f663cf52add88d0b5f5
Damien GABRIEL <damien.gabriel.fr@gmail.com>
2019-07-18 19:33:33 +0200

    Complet mais non fonctionnel
    Problème avec Firebase pour l'enregistrement des livres

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'book-manager';
+
+  constructor(){
+    var firebaseConfig = {
+      apiKey: "AIzaSyAAeB0o4tWunCp4PR-E59MBSFVqZYIHCu0",
+      authDomain: "book-manager-e5123.firebaseapp.com",
+      databaseURL: "https://book-manager-e5123.firebaseio.com",
+      projectId: "book-manager-e5123",
+      storageBucket: "book-manager-e5123.appspot.com",
+      messagingSenderId: "123625838352",
+      appId: "1:123625838352:web:6a37d88f967d51b5"
+    };
+  
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  
+  }
 }
